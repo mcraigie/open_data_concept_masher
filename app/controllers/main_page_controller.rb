e7badc243e5f2ac7963d47d61b8eeb9a00293269
mcraigie <max.craigie@gmail.com>
2017-07-01 13:32:51 +1000

    Change colours, layout and implement the tweet button

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,21 +1,34 @@
+include ActionView::Helpers::TextHelper
+
 class MainPageController < ApplicationController
+  DATASET_QUERY_LIMIT = 10
+  MINIMUM_NAME_DISTANCE = 5
+
   def random
     OpenDataset.uncached do
-      tmp = OpenDataset.order("RANDOM()").first(10)
+      tmp = OpenDataset.order("RANDOM()").first(DATASET_QUERY_LIMIT)
       a = tmp.first
       b = tmp.second
 
       loop do
-        break if Levenshtein.distance(a.name, b.name) >= 5
         b = tmp.sample
+        break if Levenshtein.distance(a.name, b.name) >= MINIMUM_NAME_DISTANCE
       end
 
       redirect_to action: "mashup", a: a.id, b: b.id
-    end    
+    end
   end
 
   def mashup
     @a = OpenDataset.find(params[:a])
     @b = OpenDataset.find(params[:b])
+
+    a_truncated = truncate(@a.name, :length => 30)
+    b_truncated = truncate(@b.name, :length => 30)
+
+    @tweet_url = "https://twitter.com/intent/tweet?text="
+    @tweet_url << ERB::Util.url_encode("#{a_truncated} + #{b_truncated}\n")
+    @tweet_url << ERB::Util.url_encode("#opendata #mashup concept\n")
+    @tweet_url << ERB::Util.url_encode(request.original_url)
   end
 end
